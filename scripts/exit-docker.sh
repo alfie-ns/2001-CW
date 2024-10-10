20fc38e5d7a13eabf66f6e23df6e4778bb777473
Alfie Nurse <alfienurse@gmail.com>
2024-10-10 17:24:59 +0100

    Trivial: formatting

--- scripts/exit-docker.sh
+++ scripts/exit-docker.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 if docker stop COMP2001sqlserv; then
     echo "Exiting Docker gracefully"
-    if osascript -e 'quit app "Docker"'; then
-        echo "Docker closed"
-    else
-        echo "Failed to close Docker"
-    fi
 else
     echo "Failed to exit Docker gracefully"
 fi
