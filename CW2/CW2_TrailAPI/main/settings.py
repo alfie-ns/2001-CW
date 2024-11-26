d886555d9e1142d1bee6bab9fc8d296785d2c270
Alfie Nurse <alfienurse@gmail.com>
2024-11-26 10:57:14 +0000

    Moderate: settings for MSSQL DB setup; implemented functionality to manage secrets in .env

--- CW2/CW2_TrailAPI/main/settings.py
+++ CW2/CW2_TrailAPI/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os # for env. variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jyrtu3p0aohba=9pi1!k7p^jb1=c%=ncwwe_6++9@2rw*a5m(5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql', # Microsoft SQL Server
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'), # university server
+        'PORT': '1433', # default port for SQL Server
+        'OPTIONS': { # additional options; not mandatory
+            'driver': 'ODBC Driver 18 for SQL Server', # SQL Server driver
+            'trustServerCertificate': 'yes', # always trust certificate
+        },
     }
 }
 
